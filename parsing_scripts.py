97696ac21860d13170a6a8d8815629ebc8437a5a
Alex Toldaiev <oleksii.toldaiev@gmail.com>
2017-03-12 04:20:59 +0000

    Better parsing functions, with indentations

--- parsing_scripts.py
+++ parsing_scripts.py
@@ -1,5 +1,32 @@
-special_tokens = [',', ';']
-indentation = '\t'
+
+examples = [ '  (a b)',
+        'a b c , d e f',
+        'a b c ; d e',
+        '(a b c ; d e)',
+        'a bc ;;; d ; (56 jj)',
+        '(a (b c ; d) e)',
+        '(a b c (d e (f (g))))',
+        'a b c , d e , f , g',
+        '(system* "ls ..")',     # Guile and shell stuff
+        '(system* "ls" "..")',
+        '(system* "ls ..")',
+        '''(for-each                                                                                                                            
+            (lambda (x) (begin (display x) (newline)))
+            (string-split (getenv "PATH") #\:))''',
+        '''for-each                                                                                                                            
+            (lambda (x) , begin (display x ; newline))
+            (string-split (getenv "PATH") #\:)''',
+        '''for-each                                                                                                                            
+            (lambda (x) , begin (display x ; newline) ;
+            string-split (getenv "PATH") #\:)'''
+        ]
+
+examples_with_indentation = [ 'a b\n\tc e\n\td',
+        '(a\nb\nc)',
+        'a b, c ; ( d\n\n\te )',
+        'a b, c ; ( d\nfoo\n\te )']
+
+special_tokens = [',', ';', '\t', '\n', '(', ')']
 
 # I cannot just tokenize
 # I need to parse indentations
@@ -68,27 +95,181 @@ def parse_line(line: str) -> str:
 
     return res
 
-examples = [ '  (a b)',
-        'a b c , d e f',
-        'a b c ; d e',
-        '(a b c ; d e)',
-        'a bc ;;; d ; (56 jj)',
-        '(a (b c ; d) e)',
-        '(a b c (d e (f (g))))',
-        'a b c , d e , f , g',
-        '(system* "ls ..")',     # Guile and shell stuff
-        '(system* "ls" "..")',
-        '(system* "ls ..")',
-        '''(for-each                                                                                                                            
-            (lambda (x) (begin (display x) (newline)))
-            (string-split (getenv "PATH") #\:))''',
-        '''for-each                                                                                                                            
-            (lambda (x) , begin (display x ; newline))
-            (string-split (getenv "PATH") #\:)''',
-        '''for-each                                                                                                                            
-            (lambda (x) , begin (display x ; newline) ;
-            string-split (getenv "PATH") #\:)'''
-        ]
 
+class Tabs:
+    def __init__(self, n):
+        self.n = n
+
+syntax_tokens = ['(', ')', '\n', '\t', ';', ',']
+
+def parse_tokens(chars: str) -> 'list(str)':
+    '''parse_tokens(line: str) -> 'list(str)'
+
+    returns a list of syntax tokens
+
+    'X ; Y'   -> ['X', ';', 'Y']
+    '(X ; Y)' -> ['(', 'X', ';', 'Y', ')']
+    'a b\nc'  -> ['a', 'b', '\n', 'c']
+    '''
+
+    for t in syntax_tokens:
+        chars = chars.replace(t, ' %s ' % t)
+
+    # merge trailing tabs after \n
+    # remove others
+
+    res = []
+    newline = False
+    tabline = 0
+    for t in chars.split(' '):
+        if t == '':
+            continue
+        elif t == '\n':
+            if newline: res += '\n'
+            newline = True
+            #res.append(t)
+        elif t == '\t': # to have all tabs caught
+            if newline:
+                tabline += 1
+            else: pass
+            # else skip the tab
+        else:
+            if tabline:
+                res += [Tabs(tabline), t]
+                newline, tabline = False, 0
+            elif newline:
+                res += ['\n', t]
+                newline = False
+            else:
+                res.append(t)
+
+    return res
+
+
+
+
+def parse_syntax(chars: str) -> str:
+    '''
+    '''
+    tokens = parse_tokens(chars)
+
+    nod_tree  = []
+    semicolon_nod = nod_tree # hook for coma-semicolon cooperation
+    nod_stack = [nod_tree] # current branch of the tree
+    open_nod, open_parenthesis = nod_tree, 0
+    prev_tabs = 0
+    # indent_parent is the last node in nod_tree == the nod_stack[1] node
+
+    for i, t in enumerate(tokens):
+        #print(len(nod_stack))
+        # cases for special tokens (ifs for now)
+        if t == '(':
+            # nest new node in the current one
+            new_nod = list()
+            nod_stack[-1].append(new_nod)
+            nod_stack.append(new_nod) # don't forget to grow the stack
+            open_nod, semicolon_nod = new_nod, new_nod
+            open_parenthesis += 1
+            # parentheses should somehow work above other syntaxis, but how?
+            #indent_parent, indent_prev, prev_tabs = nod_tree, nod_tree, 0
+
+        elif type(t) is Tabs:
+            # nest a node in the stack at depth of t.n
+            # so, I need the largest level below t.n in the stack, call it N
+            # and nest there with t.n - N depth
+            print(nod_stack)
+            if t.n <= prev_tabs:
+                # nest the parent nod of indentation
+                # it is the first after the root node nod_tree
+                # or the open_nod if it is higher in the stack..
+                # TODO: check "no-nest" option of tabs
+                while (nod_stack[-1] is not open_nod or len(nod_stack) > 2):
+                    nod_stack.pop()
+                # now nod_stack[-1] == nod_stack[1] == nod_tree[-1]
+                # or nod_stack[-1] == open_node
+                assert nod_stack[1] == nod_tree[-1] # just to be sure, it sould be true always
+
+                new_nod = list()
+                nod_stack[-1].append(new_nod)
+                nod_stack.append(new_nod)
+                semicolon_nod = new_nod
+
+            else:
+                # just nest the previous node in the stack
+                new_nod = list()
+                nod_stack[-1].append(new_nod)
+                nod_stack.append(new_nod)
+                semicolon_nod = new_nod
+            prev_tabs = t.n
+
+        elif t == '\n':
+            if open_parenthesis:
+                # probably I need to finish open tabs
+                # but remain in the opened node
+                # pop the stack untill the open_nod
+                while (nod_stack[-1] is not open_nod):
+                    nod_stack.pop()
+                semicolon_nod = nod_stack[-1]
+            # otherwise the current node parsing has ended
+            # block of tabs ended
+            # reset stuff
+            else:
+                semicolon_nod, nod_stack = nod_tree, [nod_tree]
+
+            # in any case the tabs have ended
+            prev_tabs = 0
+
+        elif t == ',':
+            # coma nests in current node, but doesn't move the semicolon hook
+            new_nod = list()
+            nod_stack[-1].append(new_nod)
+            nod_stack.append(new_nod) # don't forget to grow the stack
+        elif t == ';':
+            # semicolon finishes its' node
+            # it should move the head of the stack to the semicolon node
+            # (a ; b)? TODO: this will break on different levels of nesting
+            #  a ; b?
+            while (nod_stack[-1] is not semicolon_nod):
+                nod_stack.pop()
+            # and move to parent
+            nod_stack.pop()
+            semicolon_nod = nod_stack[-1]
+
+        elif t == ')':
+            # the parenthesis should striclty finish the corresp opening parenthesis
+            # but with the current semicolons (and indentations) it will not at some cases
+            # leaving it as is for now..
+
+            # close the node
+            # move the stack up
+            open_parenthesis -= 1
+            if open_parenthesis < 0:
+                raise SyntaxError("closing parenthesis without matchin open one, token %d" % i)
+            # otherwise,
+            # close all of the stack with the open node
+            while (nod_stack[-1] is not open_nod):
+                nod_stack.pop()
+            nod_stack.pop() # and close the open node
+            open_nod, semicolon_nod = nod_stack[-1], nod_stack[-1]
+
+        # cases for not special tokens
+        elif len(nod_stack) == 1:
+            # symbols at root-level open a new node
+            new_nod = list(t)
+            nod_stack[-1].append(new_nod)
+            nod_stack.append(new_nod)
+            semicolon_nod = new_nod
+        else:
+            # just add the symbol to the last nod
+            print(nod_stack)
+            nod_stack[-1].append(t)
+
+    return nod_tree
+
+
+def nod_tree_to_string(nod_tree: list) -> str:
+    '''
+    '''
+    return ''.join([' %s ' % nod if type(nod) is str else '(%s)' % nod_tree_to_string(nod) for nod in nod_tree])
 
 
